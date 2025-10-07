81b8add9bf1e70f9453c718e7aec680425e93428
Sergey <sergeytguzijctxur@gmail.com>
2025-10-07 16:56:55 +0300

    final one

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,9 +1,10 @@
 """Модуль для работы с текстовыми функциями."""
 
+
 def text():
     """Возвращает строку 'message'.
 
     Returns:
         str: Строка 'message'
     """
-    return "message
+    return "message"
