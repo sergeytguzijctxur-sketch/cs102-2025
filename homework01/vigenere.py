f17b4afe330396dd2fa7a1d79cf645081d43b01c
Sergey <sergeytguzijctxur@gmail.com>
2025-12-02 03:58:28 +0300

    добавил isort, black, pylint, mypy в vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -6,8 +6,8 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 
     for i, char in enumerate(plaintext):
         if char.isalpha():
-            shift = ord(keyword[i % key_length]) - ord('A')
-            base = ord('A') if char.isupper() else ord('a')
+            shift = ord(keyword[i % key_length]) - ord("A")
+            base = ord("A") if char.isupper() else ord("a")
             shifted_char = chr((ord(char) - base + shift) % 26 + base)
             ciphertext += shifted_char
         else:
@@ -24,8 +24,8 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
 
     for i, char in enumerate(ciphertext):
         if char.isalpha():
-            shift = ord(keyword[i % key_length]) - ord('A')
-            base = ord('A') if char.isupper() else ord('a')
+            shift = ord(keyword[i % key_length]) - ord("A")
+            base = ord("A") if char.isupper() else ord("a")
             original_char = chr((ord(char) - base - shift) % 26 + base)
             plaintext += original_char
         else:
