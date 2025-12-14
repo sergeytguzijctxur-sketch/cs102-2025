26af38745e7ec241453d1bb8b21e3f6d3299e629
Sergey <sergeytguzijctxur@gmail.com>
2025-12-14 19:07:40 +0300

    поправки по шифрованию

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,10 +1,23 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
+    ALPHABET_SIZE = 26
 
     for char in plaintext:
         if char.isalpha():
             base = ord("A") if char.isupper() else ord("a")
-            shifted_char = chr((ord(char) - base + shift) % 26 + base)
+            shifted_char = chr((ord(char) - base + shift) % ALPHABET_SIZE + base)
             ciphertext += shifted_char
         else:
             ciphertext += char
@@ -13,12 +26,25 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
+    ALPHABET_SIZE = 26
 
     for char in ciphertext:
         if char.isalpha():
             base = ord("A") if char.isupper() else ord("a")
-            original_char = chr((ord(char) - base - shift) % 26 + base)
+            original_char = chr((ord(char) - base - shift) % ALPHABET_SIZE + base)
             plaintext += original_char
         else:
             plaintext += char
