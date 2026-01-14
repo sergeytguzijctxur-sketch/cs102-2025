184190f351cbb80c8acd837f345b4daeb90cdf6e
Sergey <sergeytguzijctxur@gmail.com>
2026-01-14 13:07:40 +0300

    другой способ импорта

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -6,8 +6,7 @@ import random
 from pathlib import Path
 
 import pygame
-from pygame.locals import (KEYDOWN,  # pylint: disable=no-name-in-module
-                           MOUSEBUTTONDOWN, QUIT, K_l, K_p, K_q, K_r, K_s)
+from pygame.locals import *
 
 from life import GameOfLife
 from ui import UI
